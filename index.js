69fdc1cb41f7a94b16e8fb3c7fe75327931cd95f
Pranav Pavithran Sheena <1288@trenser.com>
2022-05-11 16:34:13 +0530

    stats

---
+++ index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+
+    // const markerDiv = document.createElement("a-gltf-model");
+    // markerDiv.setAttribute("rotation", "0 0 0");
+    // markerDiv.setAttribute("position", "0 0 0");
+    
+    // markerDiv.setAttribute("gltf-model", "./Ribbon.glb");
+    // markerDiv.setAttribute("scale", "6 6 6");
+
+
+    // var element = document.getElementById("#modelEntity");
+	// element.appendChild(markerDiv);
+
+ } )
